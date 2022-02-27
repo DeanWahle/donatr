55072c29efe8d080c719c8fce262efbb156f056e
Dean Wahle <60762514+DeanWahle@users.noreply.github.com>
2022-02-27 14:12:32 -0500

    Update imgur.js

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.imgur.com/3/album',
-    headers:{
-        Authorization: 'Client-ID f40bef9ff160eb3'
-    }
+  baseURL: "https://api.imgur.com/3/album",
+  headers: {
+    Authorization: "Client-ID f40bef9ff160eb3",
+  },
 });

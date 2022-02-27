7b0ac792c162404fd7390fe6c9125f241bfb9533
Dean Wahle <60762514+DeanWahle@users.noreply.github.com>
2022-02-27 14:12:15 -0500

    Update App.js

--- App.js
+++ App.js
@@ -2,22 +2,43 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
-import ListScreen from "./src/screens/ListScreen";
 import SwipeCardScreen from "./src/screens/SwipeCards";
+import ApiData from "./src/screens/ApiData";
+import React, { useState } from "react";
+import TitleScreen from "./src/screens/TitleScreen";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  useWindowDimensions,
+} from "react-native";
 
-const navigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Components: ComponentsScreen,
-    List: ListScreen,
-    Swipe: SwipeCardScreen
+const App = () => {
+  const [list, setList] = useState([]);
+  const [pressed, setPressed] = useState(false);
+
+  return list.length === 0 ? (
+    <View style={styles.card}>
+      <TitleScreen
+        pressed={pressed}
+        setPressed={setPressed}
+        list={list}
+        setList={setList}
+      />
+    </View>
+  ) : (
+    <SwipeCardScreen list={list} />
+  );
+};
+const styles = StyleSheet.create({
+  card: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 300,
+    height: 300,
   },
-  {
-    initialRouteName: "Swipe",
-    defaultNavigationOptions: {
-      title: "App",
-    },
-  }
-);
+});
 
-export default createAppContainer(navigator);
+export default App;
